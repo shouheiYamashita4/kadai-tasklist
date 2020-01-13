145ff36806ce359077921ab8c39065b2c0610f5b
shouhei.yamashita4 <shouhei.yamashita4@gmail.com>
2020-01-13 08:58:06 +0000

    change index

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -1,5 +1,24 @@
 @extends('layouts.app')
 
 @section('content')
-
+    <h1>Task List</h1>
+    
+    @if (count($tasks) > 0)
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th>id</th>
+                    <th>Task</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($tasks as $task)
+                <tr>
+                    <td>{{ $task->id }}</td>
+                    <td>{{ $task->content }}</td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    @endif
 @endsection
