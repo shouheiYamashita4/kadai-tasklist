ab04a2bb3fb3a5c61956db012b202d7ce8a5d3dd
shouhei.yamashita4 <shouhei.yamashita4@gmail.com>
2020-01-13 09:36:42 +0000

    change show

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -59,7 +59,11 @@ class TasksController extends Controller
      */
     public function show($id)
     {
-        //
+        $task = Task::find($id);
+        
+        return view('tasks.show', [
+            'task' => $task,
+        ]);
     }
 
     /**
--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -1,5 +1,16 @@
 @extends('layouts.app')
 
 @section('content')
-
+    <h1> id = {{ $task->id }} | Task detail</h1>
+    
+    <table class="table table-bordered">
+        <tr>
+            <th>id</th>
+            <td>{{ $task->id }}</td>
+        </tr>
+        <tr>
+            <th>Task</th>
+            <th>{{ $task->content }}</th>
+        </tr>
+    </table>
 @endsection
