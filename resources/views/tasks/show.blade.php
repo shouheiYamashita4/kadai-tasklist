399e0dd84e64a6babb35b12b4df93e43a44b120d
shouhei.yamashita4 <shouhei.yamashita4@gmail.com>
2020-01-13 09:50:49 +0000

    change destroy

--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -15,4 +15,8 @@
     </table>
     
     {!! link_to_route('tasks.edit', 'Modify this task' ['id' => $task->id], ['class' => 'btn btn-light']) !!}
+    
+    {!! Form::model($task, ['route' => ['tasks.destroy', $task->id], 'method' => 'delete']) !!}
+        {!! Form::submit('Delete', ['class' => 'btn btn-danger']) !!}
+    {!! Form::close() !!}
 @endsection
