b27b642651d5356104c422225ac98be5a88752d3
shouhei.yamashita4 <shouhei.yamashita4@gmail.com>
2020-01-25 11:12:30 +0000

    user signup login

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="text-center">
+        <h1>Log in</h1>
+    </div>
+    
+    <div class="row">
+        <div class="col-sm-6 offset-sm-3">
+            
+            {!! Form::open(['route' => 'login']) !!}
+                <div class="form-group">
+                    {!! Form::label('email', 'Email') !!}
+                    {!! Form::email('email', old('email'), ['class' => 'form-control']) !!}
+                </div>
+                
+                <div class="form-group">
+                    {!! Form::label('password', 'Password') !!}
+                    {!! Form::password('password', ['class' => 'form-control']) !!}
+                </div>
+                
+                {!! Form::submit('Login', ['class' => 'btn btn-primary btn-block']) !!}
+            {!! Form::close() !!}
+            
+        <p class="mt-2">New User?{!! link_to_route('signup.get', 'Sign Up Now') !!}</p>
+        </div>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,16 @@
 |
 */
 
-Route::get('/', 'TasksController@index');
+Route::get('/', function () {
+    return view('welcome');
+});
 
 Route::resource('tasks', 'TasksController');
+
+//User Registration
+Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
+Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
+//User Login
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login')->name('login.post');
+Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
